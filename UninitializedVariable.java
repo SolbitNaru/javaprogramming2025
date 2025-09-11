f4eec78184f69f870194c91b8718edd8e9cac76b
SolbitNaru <162654520+SolbitNaru@users.noreply.github.com>
2025-09-11 17:04:12 +0900

    Add files via upload

---
+++ UninitializedVariable.java
@@ -0,0 +1,7 @@
+// UninitializedVariable.java
+public class UninitializedVariable {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("num = " + num);
+    }
+}
